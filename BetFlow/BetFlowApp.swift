4dcf217685271513bb6349e6a470289cc08f99a3
kluczi <bartekcsgo04@gmail.com>
2025-05-13 23:44:25 +0200

    Initial Commit

---
+++ BetFlow/BetFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetFlowApp.swift
+//  BetFlow
+//
+//  Created by Bartek on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetFlowTests/BetFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetFlowTests.swift
+//  BetFlowTests
+//
+//  Created by Bartek on 13/05/2025.
+//
+
+import Testing
+@testable import BetFlow
+
+struct BetFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
