a5e51b4d68c7a2b9508dbb05a5c3ff4a03e38cfe
kluczi <bartekcsgo04@gmail.com>
2025-08-06 23:40:14 +0200

    replaced List to ScrollView+LazyVStack in MatchesView

--- BetFlow/Features/App/Views/MatchesView/MatchesView.swift
+++ BetFlow/Features/App/Views/MatchesView/MatchesView.swift
@@ -13,40 +13,47 @@ struct MatchesView: View {
     var body: some View {
         ZStack {
             Color.primaryBgColor.ignoresSafeArea()
-            VStack {
+            VStack (alignment: .leading) {
                 if vm.isLoading {
                     ProgressView("Loading...")
                 } else if let err = vm.errorMessage {
                     Text("err: \(err)")
                         .foregroundColor(.red)
                 } else {
-                    List(vm.events) { event in
-                        ForEach(event.bookmakers) { bookmaker in
-                            if let market = bookmaker.markets.first,
-                               market.outcomes.count == 2 {
-                                let outcome1 = market.outcomes[0]
-                                let outcome2 = market.outcomes[1]
-                                
-                                SingleMatchCard(
-                                    event: event.sportTitle,
-                                    player1: outcome1.name,
-                                    player2: outcome2.name,
-                                    odds1: outcome1.price,
-                                    odds2: outcome2.price
-                                )
-                                .padding(.horizontal,12)
-                                .padding(.bottom, 16)
-                                .listRowInsets(EdgeInsets())
-                                .listRowSeparator(.hidden)
-                                .background(.clear)
-                                .listRowBackground(Color.clear)
+                 
+                    ScrollView {
+                        LazyVStack(spacing: 12) {
+                            Text("Upcoming matches")
+                                .foregroundStyle(Color.primaryFontColor)
+                                .font(.system(size: 32))
+                                .padding(.horizontal,24)
+                                .fontWeight(.bold)
+                            ForEach(vm.events) { event in
+                                VStack(alignment: .leading, spacing: 8) {
+                                    
+                                    
+                                    ForEach(event.bookmakers) { bookmaker in
+                                        if let market = bookmaker.markets.first,
+                                           market.outcomes.count == 2 {
+                                            let o1 = market.outcomes[0]
+                                            let o2 = market.outcomes[1]
+                                            
+                                            SingleMatchCard(
+                                                event: event.sportTitle,
+                                                player1: o1.name,
+                                                player2: o2.name,
+                                                odds1: o1.price,
+                                                odds2: o2.price
+                                            )
+                                            .padding(.horizontal, 12)
+                                        }
+                                    }
+                                }
                             }
                         }
-                        
+                        .padding(.top, 12)
                     }
-                    .listStyle(.plain)
-                    .scrollContentBackground(.hidden)
-                    .background(Color.primaryBgColor)
+
                     
                 }
             }
